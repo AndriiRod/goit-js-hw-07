c9c6ba3c153bb04b8dedc44ab9ddeacd67c96b14
AndriiRod <rod.andriiw@gmail.com>
2023-04-06 23:15:08 +0300

    ready task1

---
+++ js/01-gallery.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.reduce((acc, { preview, original, description }) => {
+  return (acc += `<li class = "gallery__item"><a class = "gallery__link"><img class = "gallery__image" alt = ${description} src = ${preview} data-source = ${original} loading="lazy"></a></li>`);
+}, '');
+
+let LightboxModalImg;
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryRef.addEventListener('click', openModalImg);
+
+function closeModalImg(e) {
+  if (e.code === 'Escape') {
+    LightboxModalImg.close();
+    document.removeEventListener('keydown', closeModalImg);
+  }
+}
+
+function openModalImg(e) {
+  e.preventDefault();
+  LightboxModalImg = basicLightbox.create(`<img  src = ${e.target.dataset.source} >`);
+  LightboxModalImg.show();
+  document.addEventListener('keydown', closeModalImg);
+}
