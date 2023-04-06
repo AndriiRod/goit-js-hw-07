0df88ef6d334769846d35ba7326b7ea0b2102d96
AndriiRod <rod.andriiw@gmail.com>
2023-04-07 01:03:17 +0300

    fix task1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.reduce((acc, { preview, original, description }) => {
+  return (acc += `<li class = "gallery__item"><a class = "gallery__link" href = ${original} ><img class = "gallery__image" alt = "${description}" src = ${preview}  loading="lazy"></a></li>`);
+}, '');
+
+console.log(galleryItems[0].description);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
